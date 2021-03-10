41e6ae60abf8e50ab2fc5428304474dd11ced699
glqdlt <dlfdnd0725@gmail.com>
2021-03-10 22:57:34 +0900

    Init

---
+++ src/test/java/com/glqdlt/ex/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glqdlt.ex.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
