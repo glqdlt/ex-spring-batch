5b32fcf486804f34e499293a6e8b186510876300
glqdlt <dlfdnd0725@gmail.com>
2021-03-10 23:25:16 +0900

    기본구성

--- src/main/java/com/glqdlt/ex/springbatch/SpringBatchApplication.java
+++ src/main/java/com/glqdlt/ex/springbatch/SpringBatchApplication.java
@@ -3,6 +3,10 @@ package com.glqdlt.ex.springbatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @see <a href='https://github.com/spring-guides/gs-batch-processing'>https://github.com/spring-guides/gs-batch-processing</a>
+ * @see <a href='https://spring.io/guides/gs/batch-processing/'>https://spring.io/guides/gs/batch-processing/</a>
+ */
 @SpringBootApplication
 public class SpringBatchApplication {
 
